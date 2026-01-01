6f85d761f24297e95cb9a157dbf5ab4a4e178379
helitonns <helitonns@gmail.com>
2026-01-01 09:51:11 -0400

    Monte o layout co componente InputSingleFile - estado com arquivo

--- src/components/input-single-file.tsx
+++ src/components/input-single-file.tsx
@@ -1,7 +1,8 @@
 import { tv, type VariantProps } from "tailwind-variants";
+import FileImageIcon from "../assets/icons/image.svg?react";
 import UpdaloadFileIcon from "../assets/icons/upload-file.svg?react";
 import Icon from "./icon";
-import Text from "./text";
+import Text, { textVariants } from "./text";
 
 export const inputSingleFileVariants = tv({
   base: `
@@ -33,10 +34,12 @@ export const inputSingleFileIconVariants = tv({
 });
 
 interface InputSingleFileProps extends VariantProps<typeof inputSingleFileVariants>,
-Omit<React.ComponentProps<"input">, "size"> {}
+Omit<React.ComponentProps<"input">, "size"> {
+  error?: React.ReactNode
+}
 
 
-export default function InputSingleFile({size}: InputSingleFileProps){
+export default function InputSingleFile({size, error}: InputSingleFileProps){
   return (
     <div>
       <div className="w-full relative group cursor-pointer">
@@ -49,6 +52,28 @@ export default function InputSingleFile({size}: InputSingleFileProps){
             <br />
             ou clique para selecionar
           </Text>
+        </div>
+      </div>
+
+      {error && <Text variant="label-small" className="text-accent-red">Erro no campo</Text>}
+
+      <div className="flex gap-3 items-center border border-solid border-border-primary mt-5 p-3 rounded">
+        <Icon svg={FileImageIcon} className="fill-white w-6 h-6"/>
+        <div className="flex flex-col">
+          <div className="truncate max-w-80">
+            <Text variant="label-medium" className="text-placeholder">Nome do arquivo.png</Text>
+          </div>
+          <div className="flex">
+            <button 
+              type="button" 
+              className={textVariants({
+                variant: "label-small",
+                className: "text-accent-red cursor-pointer hover:underline"
+              })}
+            >
+              Remover
+            </button>
+          </div>
         </div>
       </div>
     </div>
