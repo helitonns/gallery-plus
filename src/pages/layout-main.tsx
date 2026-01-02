b6b7eb6b83cea9a3929f4da85675463988a24c04
helitonns <helitonns@gmail.com>
2026-01-02 10:44:38 -0400

    Crie o componente MainContent

--- src/pages/layout-main.tsx
+++ src/pages/layout-main.tsx
@@ -1,4 +1,5 @@
 import { Outlet } from "react-router";
+import MainContent from "../components/main-content";
 import MainHeader from "../components/main-header";
 
 
@@ -6,7 +7,9 @@ export default function LayoutMain(){
   return(
     <>
       <MainHeader className="mt-9"/>
-      <Outlet />
+      <MainContent>
+        <Outlet />
+      </MainContent>
     </>
   );
 }
