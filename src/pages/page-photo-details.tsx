ac08075a6cddfd27e10e416ad836074ec2b88575
helitonns <helitonns@gmail.com>
2026-01-03 10:39:50 -0400

    Crie o  mock da página de detalhes da foto - Imagem e botão de excluir

--- src/pages/page-photo-details.tsx
+++ src/pages/page-photo-details.tsx
@@ -1,5 +1,7 @@
 import { useParams } from "react-router";
+import Button from "../components/button";
 import Container from "../components/container";
+import ImagePreview from "../components/image-preview";
 import Skeleton from "../components/skeleton";
 import Text from "../components/text";
 import PhotosNavigator from "../context/photos/components/photos-navigator";
@@ -9,7 +11,7 @@ export default function PagePhotoDatails(){
   const { id } = useParams();
 
   //apenas para fazer o mock
-  const isLoadingPhoto = false;
+  const isLoadingPhoto = true;
   const photo = {
     id: "123",
     title: "Olá mundo!",
@@ -33,6 +35,23 @@ export default function PagePhotoDatails(){
 
         <PhotosNavigator />
       </header>
+
+      <div className="grid grid-cols-[21rem] gap-24">
+        <div className="space-y-3">
+          {!isLoadingPhoto ? (
+            <ImagePreview src={`/images/${photo?.imageId}`} title={photo?.title} className="h-[21rem] rounded"/>
+           ):(
+            <Skeleton className="h-[21rem]"/>
+           )
+          }
+
+          {!isLoadingPhoto ? (
+            <Button variant="destructive">Excluir</Button>
+          ) : (
+            <Skeleton className="w-20 h-10"/>
+          )}
+        </div>
+      </div>
     </Container>
   );
 }
