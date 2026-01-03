c92e48ccfd0ed960a4c9dfa8c31b8a7c1b6eec63
helitonns <helitonns@gmail.com>
2026-01-03 10:24:09 -0400

    Crie o mock da página de detalhes da foto - Header da página

--- src/pages/page-photo-details.tsx
+++ src/pages/page-photo-details.tsx
@@ -1,9 +1,38 @@
+import { useParams } from "react-router";
+import Container from "../components/container";
+import Skeleton from "../components/skeleton";
 import Text from "../components/text";
+import PhotosNavigator from "../context/photos/components/photos-navigator";
+import type { Photo } from "../context/photos/models/photo";
 
 export default function PagePhotoDatails(){
+  const { id } = useParams();
+
+  //apenas para fazer o mock
+  const isLoadingPhoto = false;
+  const photo = {
+    id: "123",
+    title: "Olá mundo!",
+    imageId: "portrait-tower.png",
+    albums: [
+      {id: "123", title: "album 1"},
+      {id: "124", title: "album 2"},
+      {id: "125", title: "album 3"},
+    ]
+  } as Photo;
+
   return(
-    <>
-      <Text variant="heading-medium">Detalhes da foto</Text>
-    </>
+    <Container>
+      <header className="flex items-center justify-between gap-8 mb-8">
+        {!isLoadingPhoto ? (
+            <Text variant="heading-large">{photo?.title}</Text>
+          ) : (
+            <Skeleton className="w-48 h-8"/>
+          )
+        }
+
+        <PhotosNavigator />
+      </header>
+    </Container>
   );
 }
