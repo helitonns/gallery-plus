5b846a35acf08fb8918bc641df3cfac69aaa6d71
helitonns <helitonns@gmail.com>
2026-01-04 15:06:43 -0400

    Adicione filtro por álbum na lista de fotos

--- src/context/album/components/albums-filter.tsx
+++ src/context/album/components/albums-filter.tsx
@@ -2,6 +2,7 @@ import cx from "classnames";
 import Button from "../../../components/button";
 import Skeleton from "../../../components/skeleton";
 import Text from "../../../components/text";
+import usePhotos from "../../photos/hooks/use-photos";
 import type { Album } from "../model/album";
 
 interface AlbumsFilterProps extends React.ComponentProps<"div"> {
@@ -10,7 +11,8 @@ interface AlbumsFilterProps extends React.ComponentProps<"div"> {
 }
 
 export default function ALbumsFilter({albums, loading, className, ...props}: AlbumsFilterProps){
-  
+  const {filters} = usePhotos();
+
   return (
     <div className={cx("flex items-center gap-3.5 overflow-x-auto", className)} {...props}>
       <Text variant="heading-small">Álbuns</Text>
@@ -18,13 +20,27 @@ export default function ALbumsFilter({albums, loading, className, ...props}: Alb
       {!loading ? (
         <>
           <div className="flex gap-3">
-            <Button variant="primary" size="sm" className="cursor-pointer">Todos</Button>
+            <Button 
+              variant={filters.albumId === null ? "primary" : "ghost"} 
+              size="sm" 
+              className="cursor-pointer"
+              onClick={()=> filters.setAlbumId(null)}
+            >
+              Todos
+            </Button>
           </div>
 
           {
             albums.map(album => (
               <div className="flex gap-3">
-                <Button key={album.id} variant="ghost" size="sm" className="cursor-pointer">{album.title}</Button>
+                <Button key={album.id} 
+                  variant={filters.albumId === album.id ? "primary" : "ghost"}  
+                  size="sm" 
+                  className="cursor-pointer"
+                  onClick={()=> filters.setAlbumId(album.id)}
+                >
+                  {album.title}
+                </Button>
               </div>
             ))
           }
--- src/context/photos/hooks/use-photos.ts
+++ src/context/photos/hooks/use-photos.ts
@@ -1,16 +1,26 @@
-import { useQuery } from "@tanstack/react-query"
-import type { Photo } from "../models/photo"
+import { useQuery } from "@tanstack/react-query";
+import { createSerializer, parseAsString, useQueryState } from "nuqs";
 import { fetcher } from "../../../helpers/api";
+import type { Photo } from "../models/photo";
 
+const toSearchParams = createSerializer({
+  albumId: parseAsString
+});
 
 export default function usePhotos(){
+  const [albumId, setAlbumId] = useQueryState("albumId");
+
   const {data, isLoading} = useQuery<Photo[]>({
-    queryKey: ["photos"],
-    queryFn: ()=> fetcher("/photos")
+    queryKey: ["photos", albumId],
+    queryFn: ()=> fetcher(`/photos${toSearchParams({albumId})}`)
   });
 
   return {
     photos: data || [],
-    isLoadingPhoto: isLoading
+    isLoadingPhoto: isLoading,
+    filters: {
+      albumId,
+      setAlbumId
+    }
   }
 }
