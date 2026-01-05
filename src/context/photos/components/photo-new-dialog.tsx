ac932a1c686d263eb22e581c72f3bb1843d3fe7d
Heliton <49277165+helitonns@users.noreply.github.com>
2026-01-05 13:22:30 +0000

    Nova foto - selecione o álbum para a foto

--- src/context/photos/components/photo-new-dialog.tsx
+++ src/context/photos/components/photo-new-dialog.tsx
@@ -4,7 +4,14 @@ import React from "react";
 import { useForm } from "react-hook-form";
 import Alert from "../../../components/alert";
 import Button from "../../../components/button";
-import { Dialog, DialogBody, DialogContent, DialogFooter, DialogHeader, DialogTrigger } from "../../../components/dialog";
+import {
+  Dialog,
+  DialogBody,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTrigger,
+} from "../../../components/dialog";
 import ImagePreview from "../../../components/image-preview";
 import InputSingleFile from "../../../components/input-single-file";
 import InputText from "../../../components/Input-text";
@@ -17,63 +24,102 @@ interface PhotoNewDialogProps {
   trigger: React.ReactNode;
 }
 
-export default function PhotoNewDialog({trigger}: PhotoNewDialogProps){
+export default function PhotoNewDialog({ trigger }: PhotoNewDialogProps) {
   const [modalOpen, setModalOpen] = React.useState(false);
   const form = useForm<PhotoNewFormSchema>({
-    resolver: zodResolver(photoNewFormSchema)
+    resolver: zodResolver(photoNewFormSchema),
   });
 
-  const {albums, isLoadingAlbums} = useAlbums();
+  const { albums, isLoadingAlbums } = useAlbums();
 
   const file = form.watch("file");
-	const fileSource = file?.[0] ? URL.createObjectURL(file[0]) : undefined;
+  const fileSource = file?.[0] ? URL.createObjectURL(file[0]) : undefined;
 
-  React.useEffect(()=> {
-    if(!modalOpen){
+  const albumIds = form.watch("albumsIds");
+
+  React.useEffect(() => {
+    if (!modalOpen) {
       form.reset();
     }
   }, [modalOpen, form]);
 
-  function handleSubmit(payload: PhotoNewFormSchema){
+  function handleToggleAlbum(albumId: string) {
+    const albumsIds = form.getValues("albumsIds") || [];
+    const albumsSet = new Set(albumsIds);
+
+    if (albumsSet.has(albumId)) {
+      albumsSet.delete(albumId);
+    } else {
+      albumsSet.add(albumId);
+    }
+
+    form.setValue("albumsIds", Array.from(albumsSet));
+  }
+
+  function handleSubmit(payload: PhotoNewFormSchema) {
     console.log(payload);
   }
 
-  return(
+  return (
     <Dialog open={modalOpen} onOpenChange={setModalOpen}>
       <DialogTrigger asChild>{trigger}</DialogTrigger>
       <DialogContent>
         <form onSubmit={form.handleSubmit(handleSubmit)}>
           <DialogHeader>Adicionar foto</DialogHeader>
-          
+
           <DialogBody className="flex flex-col gap-5">
-            <InputText placeholder="Aicione um título" maxLength={255} error={form.formState.errors.title?.message} {...form.register("title")}/>
+            <InputText
+              placeholder="Aicione um título"
+              maxLength={255}
+              error={form.formState.errors.title?.message}
+              {...form.register("title")}
+            />
             <Alert>
               Tamanho máximo: 50MB
               <br />
               Você pode selecionar arquivo rm PNG, JPG, JPEG ou WEBP
             </Alert>
 
-            <InputSingleFile 
-              form={form} 
-              allowedExtensions={["png", "jpg", "jpeg", "webp"]} 
+            <InputSingleFile
+              form={form}
+              allowedExtensions={["png", "jpg", "jpeg", "webp"]}
               maxFileSizeInMB={50}
-              replaceBy={<ImagePreview src={fileSource} className="w-full h-56"/>}
-              error={form.formState.errors.file?.message} {...form.register("file")}
+              replaceBy={
+                <ImagePreview src={fileSource} className="w-full h-56" />
+              }
+              error={form.formState.errors.file?.message}
+              {...form.register("file")}
             />
 
             <div className="space-y-3">
-              <Text variant="label-small" className="block mb-2">Selecionar álbuns</Text>
+              <Text variant="label-small" className="block mb-2">
+                Selecionar álbuns
+              </Text>
 
               <div className="flex flex-wrap gap-3">
-                {!isLoadingAlbums && albums.length > 0 && albums.map(album => 
-                  <Button key={album.id} variant="ghost" size="sm" className="truncate">
-                    {album.title}
-                  </Button>
-                )}
+                {!isLoadingAlbums &&
+                  albums.length > 0 &&
+                  albums.map((album) => (
+                    <Button
+                      key={album.id}
+                      variant={
+                        albumIds?.includes(album.id) ? "primary" : "ghost"
+                      }
+                      size="sm"
+                      className="truncate"
+                      onClick={() => handleToggleAlbum(album.id)}
+                    >
+                      {album.title}
+                    </Button>
+                  ))}
 
-                {isLoadingAlbums && Array.from({length: 5}).map((_, index) => 
-                  <Skeleton key={`album-loading-${index}`} className="w-20 h-7"/>
-                )}
+                {isLoadingAlbums &&
+                  Array.from({ length: 5 }).map((_, index) => (
+                    <Skeleton
+                      key={`album-loading-${index}`}
+                      className="w-20 h-7"
+                    />
+                  ))}
               </div>
             </div>
           </DialogBody>
@@ -84,7 +130,6 @@ export default function PhotoNewDialog({trigger}: PhotoNewDialogProps){
             </DialogClose>
             <Button type="submit">Adicionar</Button>
           </DialogFooter>
-
         </form>
       </DialogContent>
     </Dialog>
